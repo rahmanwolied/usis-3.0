772f951a011d6a7c446de40c388a776ed885de7a
Niloy <nafis.sadique.niloy@g.bracu.ac.bd>
2024-07-06 04:02:06 +0600

    chore: Update mailer to use set method for updating reset password code and expiration

--- src/utilities/mailer.ts
+++ src/utilities/mailer.ts
@@ -8,7 +8,7 @@ export const sendEmail = async ({ email, mailtype, userID }: any) => {
 		if (mailtype === 'verify') {
 			await User.findByIdAndUpdate(userID, { verifyCode: hashedToken, verifyCodeExpiration: Date.now() + 3600000 });
 		} else if (email === 'reset') {
-			await User.findByIdAndUpdate(userID, { resetPasswordCode: hashedToken, resetPasswordCodeExpiration: Date.now() + 3600000 });
+			await User.findByIdAndUpdate(userID, { set: { resetPasswordCode: hashedToken, resetPasswordCodeExpiration: Date.now() + 3600000 } });
 		}
 
 		const transport = nodemailer.createTransport({
@@ -22,8 +22,10 @@ export const sendEmail = async ({ email, mailtype, userID }: any) => {
 		const mailOptions = {
 			from: process.env.EMAIL_USER,
 			to: email,
+
+			//check this line of code ********************
 			subject: mailtype === 'verify' ? 'Verify your email' : 'Reset your password',
-			html: "<p>Click <a href='${process.env.DOMAIN}/verifyemail?token=${hashedToken}'>here</a> to $ {mailtype === 'Verify'? 'verify your email': 'Reset your password'} or copy and paste the link belwo in your braower.<br>${process.env.DOMAIN}/verifyemail?token=${hashedToken} </p>",
+			html: `<p>Click <a href='${process.env.DOMAIN}/verifyemail?token=${hashedToken}'>here</a> to $ {mailtype === 'Verify'? 'verify your email': 'Reset your password'} or copy and paste the link belwo in your braower.<br>${process.env.DOMAIN}/verifyemail?token=${hashedToken} </p>`,
 		};
 		const mailResponse = await transport.sendMail(mailOptions);
 	} catch (error: any) {
