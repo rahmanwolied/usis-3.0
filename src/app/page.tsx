b76d45ccb723206d3a17d795b4051fb9be5a0afe
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-06 03:45:43 +0600

    chore: Update npm dependencies and add Google authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Page() {
-	return <div>Page</div>;
+	return (
+		<div className="flex h-screen items-center justify-center gap-5">
+			<Link href="/signup">
+				<Button>Signup</Button>
+			</Link>
+			<Link href="/login">
+				<Button>Login</Button>
+			</Link>
+		</div>
+	);
 }
