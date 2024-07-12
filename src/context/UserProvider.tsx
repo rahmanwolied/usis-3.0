276e1f136c9f50c28c2e5ee2a826ea88a78cd773
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-12 17:14:04 +0600

    chore: Create UserProvider context for managing user state

---
+++ src/context/UserProvider.tsx
@@ -0,0 +1,7 @@
+import { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext({});
+export const UserContextProvider = ({ children }:{children:}) => {
+	const [user, setUser] = useState(null);
+	return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
