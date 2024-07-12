0c01eb0235a13f33201db4995ca8ef756b3247fc
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-12 17:14:40 +0600

    feat: Add lazy loading for images in signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,11 +4,13 @@ import { signIn } from 'next-auth/react';
 import GoogleButton from 'react-google-button';
 import SignupForm from './components/signup-form'; // Adjust the path as necessary
 import Footer from './components/footer'; // Adjust the path as necessary
+import Image from 'next/image';
 
 function Page() {
 	return (
 		<form>
 			<div className="h-screen flex flex-col items-center justify-center space-y-4">
+				<Image src="/static/usis-logo.png" width={100} height={100} alt="usis"></Image>
 				<SignupForm />
 				<GoogleButton onClick={() => signIn('google')} />
 				<Footer />
