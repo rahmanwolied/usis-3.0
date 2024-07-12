e3738e89f8dd9660414ad279d0e15793d4ee2092
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-12 17:14:32 +0600

    chore: Refactor signup route and add user verification functionality

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,43 +1,47 @@
-import { connect } from '@/lib/dbConfig';
-import User from '@/model/User';
+import { dbConnect } from '@/lib/dbConfig';
+import UserModel, { User } from '@/model/User';
 import { NextRequest, NextResponse } from 'next/server';
-import bcryptjs from 'bcryptjs';
 import { sendEmail } from '@/utilities/mailer';
+import { ApiResponse } from '@/../types/ApiResponse.type';
+import { hashPassword } from '@/lib/hashPassword';
+import { Document } from 'mongoose';
 
 export async function POST(request: NextRequest) {
-	connect();
+	await dbConnect();
 	try {
 		const reqBody = await request.json();
 		const { username, email, password, image, name } = reqBody;
 		//Validation
 		console.log(reqBody);
 
-		const user = await User.findOne({ email });
+		const user = await UserModel.findOne({ email, username });
 		if (user) {
-			return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+			return NextResponse.json<ApiResponse<null>>({ status: 'error', message: 'User already exists' }, { status: 400 });
 		}
+		const hashedPassword = await hashPassword(password);
+		const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+		const verifyCodeExpiration = Date.now() + 5 * 60 * 1000;
 
-		const salt = await bcryptjs.genSalt(10);
-		const hashedPassword = await bcryptjs.hash(password, salt);
-
-		const newUser = new User({
-			username: username ? username : email.split('@')[0],
+		const newUser = new UserModel({
+			username: username ?? '',
 			email,
 			password: hashedPassword,
-			image: image ? image : undefined,
-			name: name ? name : '',
+			image: image ?? undefined,
+			name: name ?? '',
+			verifyCode,
+			verifyCodeExpiration: new Date(verifyCodeExpiration),
 		});
 		const savedUser = await newUser.save();
 		console.log(savedUser);
 
 		//send verify email
-		await sendEmail({ email, mailtype: 'verify', userID: savedUser._id });
-		return NextResponse.json({
+		const emailResponse = await sendEmail(email, savedUser._id, parseInt(verifyCode));
+		return NextResponse.json<ApiResponse<Document>>({
 			message: 'User Registered Successfully',
-			success: true,
-			savedUser,
+			status: 'success',
+			data: savedUser,
 		});
 	} catch (error: any) {
-		return NextResponse.json({ error: error.message }, { status: 500 });
+		return NextResponse.json<ApiResponse<null>>({ message: error.message, status: 'error' }, { status: 500 });
 	}
 }
