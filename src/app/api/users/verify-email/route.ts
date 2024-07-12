07e335a632862f9d8eaf0eb4d0e8ae1a6030225f
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-12 17:12:54 +0600

    chore: Refactor verify-email route to use dbConnect instead of connect

--- src/app/api/users/verify-email/route.ts
+++ src/app/api/users/verify-email/route.ts
@@ -1,21 +1,28 @@
-import { connect } from '@/lib/dbConfig';
+import { dbConnect } from '@/lib/dbConfig';
 import User from '@/model/User';
 import { NextRequest, NextResponse } from 'next/server';
 
-connect();
-
 export async function POST(request: NextRequest) {
 	try {
+		await dbConnect();
 		const reqBody = await request.json();
-		const { token } = reqBody;
+		const { username, token } = reqBody;
 		console.log(token);
 
-		const user = await User.findOne({ verifyCode: token, verifyCodeExpiration: { $gt: Date.now() } });
+		const user = await User.findOne({ username });
 
 		if (!user) {
-			return NextResponse.json({ error: 'Invalid code or code expired' }, { status: 500 });
+			return NextResponse.json({ status: 'error', message: 'User not found!' }, { status: 404 });
 		}
 		console.log(user);
+
+		if (user.verifyCode !== token) {
+			return NextResponse.json({ status: 'error', message: 'Invalid token!' }, { status: 400 });
+		}
+
+		if (user.verifyCodeExpiration! < new Date(Date.now())) {
+			return NextResponse.json({ status: 'error', message: 'Token expired!' }, { status: 400 });
+		}
 
 		user.isVerified = true;
 		user.verifyCode = null;
