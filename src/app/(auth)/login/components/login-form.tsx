9d8938c47d71df949da1955f919b552eefee4974
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-07-16 04:23:29 +0600

    chore: Refactor login form component to use form library and form inputs

--- src/app/(auth)/login/components/login-form.tsx
+++ src/app/(auth)/login/components/login-form.tsx
@@ -1,49 +1,36 @@
 import React, { useState } from 'react';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Button } from '@/components/ui/button';
-import axios from 'axios';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input, PasswordInput } from '@/components/ui/input';
 
-export default function LoginForm() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [showPassword, setShowPassword] = useState(false); // New state to toggle password visibility
-
-	const handleSubmit = async (event: any) => {
-		event.preventDefault();
-		const res = await axios.post('/api/users/login', {
-			email,
-			password,
-		});
-		console.log(res);
-	};
-
+export default function LoginForm({ form }: { form: any }) {
 	return (
 		<>
-			<h2 className="text-center font-bold text-2xl mb-4">Usis 3.0 Login</h2>
-			<form className="flex flex-col gap-3">
-				<label htmlFor="email">Email:</label>
-				<input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border p-2 rounded" />
-
-				<label htmlFor="password">Password:</label>
-				<div className="flex items-center border p-2 rounded">
-					<input
-						type={showPassword ? 'text' : 'password'} // Toggle between text and password
-						id="password"
-						name="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						className="flex-1"
-					/>
-
-					<button type="button" onClick={() => setShowPassword(!showPassword)} className="ml-2">
-						{showPassword ? <FaEyeSlash /> : <FaEye />}
-					</button>
-				</div>
-
-				<Button onClick={handleSubmit} className="mt-4">
-					Log In
-				</Button>
-			</form>
+			<FormField
+				control={form.control}
+				name="identifier"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Email</FormLabel>
+						<FormControl>
+							<Input {...field} />
+						</FormControl>
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={form.control}
+				name="password"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Password</FormLabel>
+						<FormControl>
+							<PasswordInput {...field} />
+						</FormControl>
+					</FormItem>
+				)}
+			/>
 		</>
 	);
 }
+
+LoginForm.displayName = 'LoginForm';
