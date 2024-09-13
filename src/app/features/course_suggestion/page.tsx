12275b3c0e9c85c35201f877bea29ee3249676bf
NabanitaSarker <nabanitasarker18@gmail.com>
2024-09-13 18:49:21 +0600

    nabanita course suggestion 1

---
+++ src/app/features/course_suggestion/page.tsx
@@ -0,0 +1,22 @@
+
+
+
+function App() {
+
+
+  const fetchGradesheet = async () => {
+
+  };
+
+  const fetchAdvisedCourses = async () => {
+
+  };
+
+
+
+  return (
+    <h1>Course Suggestion</h1>
+  );
+}
+
+export default App;
---
+++ src/app/features/course_suggestion/prereq.ts
@@ -0,0 +1,157 @@
+export const prereq = [
+    {
+        "Course": "CSE111",
+        "Pre-Requisite": "CSE110",
+        "Full Chain Pre-Requisite": "CSE110"
+    },
+    {
+        "Course": "CSE220",
+        "Pre-Requisite": "CSE111,CSE230",
+        "Full Chain Pre-Requisite": "CSE230-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE221",
+        "Pre-Requisite": "CSE220",
+        "Full Chain Pre-Requisite": "CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE250",
+        "Pre-Requisite": "PHY112",
+        "Full Chain Pre-Requisite": "PHY112"
+    },
+    {
+        "Course": "CSE251",
+        "Pre-Requisite": "CSE250",
+        "Full Chain Pre-Requisite": "CSE250"
+    },
+    {
+        "Course": "CSE260",
+        "Pre-Requisite": "CSE251",
+        "Full Chain Pre-Requisite": "CSE251-CSE250"
+    },
+    {
+        "Course": "CSE310",
+        "Pre-Requisite": "CSE370",
+        "Full Chain Pre-Requisite": "CSE370-CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE321",
+        "Pre-Requisite": "CSE221",
+        "Full Chain Pre-Requisite": "CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE330",
+        "Pre-Requisite": "MAT216",
+        "Full Chain Pre-Requisite": "MAT120-MAT110"
+    },
+    {
+        "Course": "CSE331",
+        "Pre-Requisite": "CSE221",
+        "Full Chain Pre-Requisite": "CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE340",
+        "Pre-Requisite": "CSE260",
+        "Full Chain Pre-Requisite": "CSE260-CSE251-CSE250"
+    },
+    {
+        "Course": "CSE341",
+        "Pre-Requisite": "CSE340,CSE321",
+        "Full Chain Pre-Requisite": ""
+    },
+    {
+        "Course": "CSE350",
+        "Pre-Requisite": "CSE251",
+        "Full Chain Pre-Requisite": "CSE251-CSE250"
+    },
+    {
+        "Course": "CSE360",
+        "Pre-Requisite": "CSE341",
+        "Full Chain Pre-Requisite": "CSE341"
+    },
+    {
+        "Course": "CSE370",
+        "Pre-Requisite": "CSE221",
+        "Full Chain Pre-Requisite": "CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE410",
+        "Pre-Requisite": "CSE321",
+        "Full Chain Pre-Requisite": "CSE321-CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE420",
+        "Pre-Requisite": "CSE321,CSE331,CSE340",
+        "Full Chain Pre-Requisite": "CSE321-CSE331-CSE340-CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE421",
+        "Pre-Requisite": "CSE320",
+        "Full Chain Pre-Requisite": ""
+    },
+    {
+        "Course": "CSE422",
+        "Pre-Requisite": "CSE221",
+        "Full Chain Pre-Requisite": "CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE423",
+        "Pre-Requisite": "MAT216",
+        "Full Chain Pre-Requisite": "MAT216-MAT120-MAT110"
+    },
+    {
+        "Course": "CSE430",
+        "Pre-Requisite": "MAT120",
+        "Full Chain Pre-Requisite": "MAT120-MAT110"
+    },
+    {
+        "Course": "CSE460",
+        "Pre-Requisite": "CSE260",
+        "Full Chain Pre-Requisite": "CSE260-CSE251-CSE250"
+    },
+    {
+        "Course": "CSE461",
+        "Pre-Requisite": "CSE260",
+        "Full Chain Pre-Requisite": "CSE260-CSE251-CSE250"
+    },
+    {
+        "Course": "CSE470",
+        "Pre-Requisite": "CSE370",
+        "Full Chain Pre-Requisite": "CSE370-CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "CSE471",
+        "Pre-Requisite": "CSE370",
+        "Full Chain Pre-Requisite": "CSE370-CSE221-CSE220-CSE111-CSE110"
+    },
+    {
+        "Course": "ENG102",
+        "Pre-Requisite": "ENG101",
+        "Full Chain Pre-Requisite": "ENG101"
+    },
+    {
+        "Course": "ENG103",
+        "Pre-Requisite": "ENG102",
+        "Full Chain Pre-Requisite": "ENG102"
+    },
+    {
+        "Course": "MAT120",
+        "Pre-Requisite": "MAT110",
+        "Full Chain Pre-Requisite": "MAT110"
+    },
+    {
+        "Course": "MAT215",
+        "Pre-Requisite": "MAT216",
+        "Full Chain Pre-Requisite": "MAT216-MAT120-MAT110"
+    },
+    {
+        "Course": "MAT216",
+        "Pre-Requisite": "MAT120",
+        "Full Chain Pre-Requisite": "MAT120-MAT110"
+    },
+    {
+        "Course": "PHY112",
+        "Pre-Requisite": "PHY111",
+        "Full Chain Pre-Requisite": "PHY111"
+    }
+]
