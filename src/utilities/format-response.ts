db48bf47c967a1fedb2d0abb7de83cd6acd8d31b
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-09-20 01:48:30 +0600

    feature: Add getClassroomFromSchedule

--- src/utilities/format-response.ts
+++ src/utilities/format-response.ts
@@ -1,106 +1,247 @@
 import { Days, Times } from '@/enums';
+import UsisSDK from '@/services/usis-sdk';
+import parse from 'node-html-parser';
 
-import type { Course, Section, UsisScheduleResponseType } from '@/types/usisReponse.type';
+import type {
+    Course,
+    Section,
+    USISClassScheduleWithLabs,
+    UsisScheduleResponseType,
+} from '@/types/usisReponse.type';
 
-function _toNumber(section: string) {
-    if (section.startsWith('S')) {
-        section = section.slice(1);
-    }
-    return Number(section.slice(0, 2));
-}
+import {
+    _toNumber,
+    extractAndCombineTimes,
+    extractDayTimeRoom,
+} from './helpers';
 
-// Just to get all the unique start and end times for writing the types
-function sortTimes(response: UsisScheduleResponseType): void {
-    const AMtimes: string[] = [];
-    const PMtimes: string[] = [];
-
-    const startTimes: string[] = [];
-    const endTimes: string[] = [];
-
-    const timePairs = response.rows.forEach((row) => {
-        let [rowCount, id, courseCode, facultyInitial, section, courseDay, startTime, endTime, roomNumber] = row.cell;
-
-        let stTime = startTime.slice(0, 5);
-        let enTime = endTime.slice(0, 5);
-
-        if (!startTimes.includes(startTime)) startTimes.push(startTime);
-        if (!endTimes.includes(endTime)) endTimes.push(endTime);
-
-        if (startTime.includes('AM') && !AMtimes.includes(stTime)) AMtimes.push(stTime);
-        if (endTime.includes('AM') && !AMtimes.includes(enTime)) AMtimes.push(enTime);
-
-        if (startTime.includes('PM') && !PMtimes.includes(stTime)) PMtimes.push(stTime);
-        if (endTime.includes('PM') && !PMtimes.includes(enTime)) PMtimes.push(enTime);
-    });
-    console.log(
-        'startTimes Sorted',
-        startTimes.sort(
-            (a, b) => Number(a.replace(':', '').replace('AM', '').replace('PM', '')) - Number(b.replace(':', '').replace('AM', '').replace('PM', '')),
-        ),
-    );
-    console.log(
-        'endTimes Sorted',
-        endTimes.sort(
-            (a, b) => Number(a.replace(':', '').replace('AM', '').replace('PM', '')) - Number(b.replace(':', '').replace('AM', '').replace('PM', '')),
-        ),
-    );
-}
-
-export function formatClassScheduleResponse(response: UsisScheduleResponseType): Course[] {
+export function formatClassScheduleWithLab(
+    rows: USISClassScheduleWithLabs[],
+    classRoomInfo: [string, string, string[], string[], string[], string[]][],
+): Course[] {
     const courses: Course[] = [];
-    response.rows.forEach((row) => {
-        let [rowCount, id, courseCode, facultyInitial, section, courseDay, startTime, endTime, roomNumber] = row.cell;
-        let tarc = false;
-        let closed = false;
-        if (section.includes('CLOSED')) {
-            closed = true;
-        }
-        if (section.startsWith('S')) {
-            tarc = true;
+    rows.forEach((row) => {
+        const [
+            rowCount,
+            courseCode,
+            courseTitle,
+            section,
+            seats,
+            department,
+            program,
+            facultyName,
+            facultyInitial,
+            examDate,
+            examDay,
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+        ] = row.cell;
+
+        const isTarc = section.startsWith('S');
+        const isClosed = section.includes('CLOSED');
+
+        const _days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+
+        const days: string[] = [];
+        const startTimes: string[] = [];
+        const endTimes: string[] = [];
+
+        for (let i = 11; i < 18; i++) {
+            if (row.cell[i]) {
+                days.push(_days[i - 11]);
+                const { startTime, endTime } = extractAndCombineTimes(
+                    row.cell[i] as string,
+                );
+                startTimes.push(startTime);
+                endTimes.push(endTime);
+            }
         }
 
-        const addedCourse = courses.find((course) => course.code === courseCode);
         const sectionInfo: Section = {
-            days: [Days[courseDay]],
-            startTime: [Times[startTime]],
-            endTime: [Times[endTime]],
+            days,
+            startTimes,
+            endTimes,
             facultyInitial,
-            number: id,
-            section,
-            roomNumber,
-            closed,
+            number: section,
+            roomNumber: '',
+            closed: isClosed,
+            examDate,
+            examDay,
+            facultyName,
         };
 
+        const addedCourse = courses.find(
+            (course) => course.code === courseCode,
+        );
         if (!addedCourse) {
             const course = {} as Course;
             course.code = courseCode;
-            course.tarc = tarc;
+            course.tarc = isTarc;
+            course.title = courseTitle;
+            course.faculties = [facultyInitial];
             course.sections = [sectionInfo];
             courses.push(course);
             return;
         }
-        const addedSection = addedCourse.sections.find((section) => section.number === id);
+        const addedSection = addedCourse.sections.find(
+            (_section) => _section.number === section,
+        );
 
         if (!addedSection) {
             addedCourse.sections.push(sectionInfo);
-            addedCourse.sections.sort((a, b) => _toNumber(a.section) - _toNumber(b.section));
+            addedCourse.sections.sort(
+                (a, b) => _toNumber(a.number) - _toNumber(b.number),
+            );
             return;
         }
-
-        const dayIdx = addedSection.days.findIndex((day) => day === Days[courseDay]);
-
-        if (dayIdx === -1 || Math.abs(addedSection.endTime[dayIdx] - Times[startTime]) > 1) {
-            addedSection.days.push(Days[courseDay]);
-            addedSection.startTime.push(Times[startTime]);
-            addedSection.endTime.push(Times[endTime]);
-
-            return;
-        }
-
-        addedSection.startTime[dayIdx] = Math.min(addedSection.startTime[dayIdx], Times[startTime]);
-        addedSection.endTime[dayIdx] = Math.max(addedSection.endTime[dayIdx], Times[endTime]);
 
         return;
     });
     return courses;
 }
+
+export function getClassroomsFromSchedule(html: string) {
+    const document = parse(html);
+    const rows = document.querySelectorAll('#customers tbody tr');
+    const result = Array<{
+        courseCode: string;
+        sections: {
+            section: string;
+            days: string[];
+            startTimes: string[];
+            endTimes: string[];
+            roomNumbers: string[];
+        }[];
+    }>();
+
+    rows.forEach((row) => {
+        const cells = row.querySelectorAll('td');
+        const courseCode = cells[1].textContent.trim();
+
+        const section = cells[5].textContent.trim();
+        const dayTimeRoom = cells[6].textContent.trim();
+        const { days, startTimes, endTimes, roomNumbers } =
+            extractDayTimeRoom(dayTimeRoom);
+
+        const existingCourse = result.find(
+            (course) => course.courseCode === courseCode,
+        );
+        if (!existingCourse)
+            result.push({
+                courseCode,
+                sections: [
+                    {
+                        section,
+                        days,
+                        startTimes,
+                        endTimes,
+                        roomNumbers,
+                    },
+                ],
+            });
+        else {
+            existingCourse.sections.push({
+                section,
+                days,
+                startTimes,
+                endTimes,
+                roomNumbers,
+            });
+        }
+    });
+    return result;
+}
+
+// export function formatClassScheduleResponse(
+//     response: UsisScheduleResponseType,
+// ): Course[] {
+//     const courses: Course[] = [];
+//     response.rows.forEach((row) => {
+//         let [
+//             rowCount,
+//             id,
+//             courseCode,
+//             facultyInitial,
+//             number,
+//             courseDay,
+//             startTime,
+//             endTime,
+//             roomNumber,
+//         ] = row.cell;
+//         let isTarc = number.startsWith('S');
+//         let isClosed = number.includes('CLOSED');
+
+//         const addedCourse = courses.find(
+//             (course) => course.code === courseCode,
+//         );
+
+//         const sectionInfo: Section = {
+//             days: [courseDay],
+//             startTimes: [startTime],
+//             endTimes: [endTime],
+//             facultyInitial,
+//             number,
+//             roomNumber,
+//             closed: isClosed,
+//         };
+
+//         if (!addedCourse) {
+//             const course = {} as Course;
+//             course.code = courseCode;
+//             course.tarc = isTarc;
+//             course.sections = [sectionInfo];
+//             course.faculties = [facultyInitial];
+//             courses.push(course);
+//             return;
+//         }
+//         const addedSection = addedCourse.sections.find(
+//             (section) => section.number === number,
+//         );
+
+//         if (!addedSection) {
+//             addedCourse.sections.push(sectionInfo);
+//             addedCourse.sections.sort(
+//                 (a, b) => _toNumber(a.number) - _toNumber(b.number),
+//             );
+//             return;
+//         }
+
+//         const dayIdx = addedSection.days.findIndex((day) => day === courseDay);
+
+//         if (
+//             dayIdx === -1 ||
+//             Math.abs(addedSection.endTimes[dayIdx] - Times[startTime]) > 1
+//         ) {
+//             addedSection.days.push(courseDay);
+//             addedSection.startTimes.push(Times[startTime]);
+//             addedSection.endTimes.push(Times[endTime]);
+
+//             return;
+//         }
+
+//         addedSection.startTime[dayIdx] = Math.min(
+//             addedSection.startTime[dayIdx],
+//             Times[startTime],
+//         );
+//         addedSection.endTime[dayIdx] = Math.max(
+//             addedSection.endTime[dayIdx],
+//             Times[endTime],
+//         );
+
+//         return;
+//     });
+//     return courses;
+// }
