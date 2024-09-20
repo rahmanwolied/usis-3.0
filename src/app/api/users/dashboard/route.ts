eaa00e4bf0905751e32e8aaa58a151012ae1fa2e
rahmanwolied <mosheur.r.wolied@gmail.com>
2024-09-21 01:05:33 +0600

    Refactor course suggestion page to fetch course information from the server

--- src/app/api/users/dashboard/route.ts
+++ src/app/api/users/dashboard/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import UserModel from '@/model/User';  // Import the User model
-import { CourseModel } from '@/model/Course';  // Import the Course model
+import { CourseModel } from '@/model/Course';
+import UserModel from '@/model/User';
 
 import { ApiResponse } from '@/types/ApiResponse.type';
 import dbConnect from '@/lib/dbConnect';
@@ -10,10 +10,8 @@ export async function POST(req: NextRequest) {
     const { email } = await req.json();
 
     try {
-        // Fetch the user's advised courses
-
+        console.log('Fetching courses for:', email);
         const user = await UserModel.findOne({ email }).lean();
-        console.log(user);
         if (!user || !user.advisedCourses) {
             return NextResponse.json({ error: 'No advised courses found' });
         }
@@ -25,32 +23,39 @@ export async function POST(req: NextRequest) {
             advisedCourses.map(async (course) => {
                 const courseData = await CourseModel.findOne({
                     code: course.code,
-                    'sections.section': course.section
+                    'sections.section': course.section,
                 }).lean();
 
                 if (courseData) {
-                    const section = courseData.sections.find((sec) => sec.section === course.section);
+                    const section = courseData.sections.find(
+                        (sec) => sec.section === course.section,
+                    );
                     return {
                         code: courseData.code,
                         title: courseData.title,
                         faculty: section?.facultyName,
                         section: section?.section,
-                        lab: section?.lab
+                        lab: section?.lab,
                     };
                 }
                 return null;
-            })
+            }),
         );
 
         // Fetch the completed credits from grades
-        const completedCredits = user.grades.reduce((acc, grade) => acc + parseFloat(grade.creditEarned), 0);
+        const completedCredits = user.grades.reduce(
+            (acc, grade) => acc + parseFloat(grade.creditEarned),
+            0,
+        );
 
         return NextResponse.json({
             currentCourses: coursesWithSections.filter(Boolean),
-            completedCredits
+            completedCredits,
         });
     } catch (error) {
         console.error('Error fetching courses:', error);
-        return NextResponse.json({ error: 'An error occurred while fetching the data' });
+        return NextResponse.json({
+            error: 'An error occurred while fetching the data',
+        });
     }
 }
